b997a39989cd8af47af7aa5ca6cbe66607677013
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-06-10 14:54:40 -0700

    test 1 return an array of numbers from 0 to the user's inputted number has passed

--- README.md
+++ README.md
@@ -69,3 +69,14 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE._
 
 Copyright (c) June 10th, 2022 Joe Roaden
+
+_Tests_
+
+Describe: beepBoop()
+
+Test: "It should return an array of numbers from 0 to the user's inputted number"
+Code: beepBoop(5);
+Expected Output: [0, 1, 2, 3, 4, 5]
+
+Test: "It should...
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 //Utility Logic
+function beepBoop(element) {
 
+}
 
 
 
 
 //Business Logic
-  function beepBoop(number) {
-    const number = 5
-    let numberArray = [];
-    for (let index = 0; index <= number.length ; index +=1) {
-      numberArray.push(index);
-    }
-  }
+  let ourArray = [];
+  for (let i = 0; i < 5; i++) {
+    ourArray.push(i); }
+
+
+
+
+//function beepBoop(number) {
+    //let number = 5
+    //let numberArray = [];
+    //for (let index = 0; index <= parseInt(number).length ; index +=1) {
+      //numberArray.push(index);
+    
+  
 
   
   
