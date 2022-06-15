530d674e7a0625c601ac09c46399f8eb2b1ec3da
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-06-15 07:26:34 -0700

    WIP - refator code to pass test one

--- js/scripts.js
+++ js/scripts.js
@@ -6,30 +6,27 @@ let wontYou = "Won't you be my neighbor?"
 
 
 //Business Logic
-let beepBoop = function(number1) {
-  let array = [];
-  for (let i = 0; i <= number1; i++) {
-      array.push(i.toString());
+ function beepBoop(number1) {
+  let array = "";
+  let result = [];
+  for (let i = 0; i <= number1; i+=1) {
+    
   };
 }
-return array
-    
-    //return beepBoop;
-    //if (array[i] == 1 {
-          //array + "Beep!"
-    //}
+//return result;
 
     
+   
   
 
 //UI Logic//
   $(document).ready(function(){
-    $("form#counter").submit(function(event){
+    $("form#counter").submit(function(event) {
     event.preventDefault();
-    const input1 = $("#number1").val ();
+    const input1 = $("#number1").val();
     const result = beepBoop(input1);
     $("#output").html(result + "");
-    $("#output").click(function() {
-      $(this).remove();
+   
     });
   });
+
