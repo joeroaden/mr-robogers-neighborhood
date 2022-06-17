e1f8fee0bea3f26f8af6fcfed1f841a858ca4517
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-06-16 22:02:35 -0700

    prettify formatting to correct indentation issues

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,33 @@
-//Utility Logic//
-
-
-
-
 //Business Logic
- let beepBoop = function(input) {
+function beepBoop(input) {
   let results = [];
   for (let i = 0; i <= input; i++) {
     results.push(i.toString());
-  };
-
-
- 
-let beepBoopBeep = results.map(function(array) {
-  let beep = "Beep!";
-  let boop = "Boop!";
-  let neighbor = "Won't you be my neighbor?";
-  if (array.includes("3")) {
-    return array = neighbor;
   }
-  else if (array.includes("2")) {
-    return array = boop;
-  }
-  else if (array.includes("1")) {
-    return array = beep;
-  } else {
-    return array;
-  };
-});
+
+  let beepBoopBeep = results.map(function (array) {
+    let beep = "Beep!";
+    let boop = "Boop!";
+    let neighbor = "Won't you be my neighbor?";
+    if (array.includes("3")) {
+      return (array = neighbor);
+    } else if (array.includes("2")) {
+      return (array = boop);
+    } else if (array.includes("1")) {
+      return (array = beep);
+    } else {
+      return array;
+    }
+  });
   return beepBoopBeep;
- };
-
-    
-   
-  
+}
 
 //UI Logic//
-  $(document).ready(function(){
-    $("form#counter").submit(function(event) {
+$(document).ready(function () {
+  $("form#counter").submit(function (event) {
     event.preventDefault();
     const input1 = parseInt($("#number1").val());
     const resultFinal = beepBoop(input1);
     $("#output").text(resultFinal);
-    });
   });
-
-  //$("input#new-home-address").val("");//  to reset after click
+});
